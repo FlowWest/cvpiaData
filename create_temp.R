c5dd6b98bc038647fb70bf6a61578e59d094366e
thesadie <thesadie@gmail.com>
2017-04-24 16:00:02 -0700

    example use of get_temperature_data

--- create_temp.R
+++ create_temp.R
@@ -4,13 +4,6 @@ end_year = 1989
 
 
 # create temp input
-temp <- CVPIAdata::temperatures %>%
-  dplyr::filter(!(watershed %in% c('SC.Delta', 'N.Delta')),
-                year >= start_year & year <= end_year) %>%
-  tidyr::unite(date, year, month) %>%
-  tidyr::spread(date, avg_temp) %>%
-  dplyr::left_join(CVPIAdata::watershed_ordering) %>%
-  dplyr::arrange(order) %>%
-  dplyr::select(-watershed, -order)
 
-juv_tmp <- create_DSM_array(temps)
+juv_tmp <- get_temperature_data(start_year, end_year)
+juv_tmp_delta <- get_temperature_data(start_year, end_year, 'delta')
