8bb66e6e51a017c7e31b0911d51e470e34bb7a7d
thesadie <thesadie@gmail.com>
2017-04-26 15:53:44 -0700

    break out set_sit_data function

---
+++ CVPIAdata.R
@@ -0,0 +1,11 @@
+#' CVPIAdata: A package for generating inputs to the SIT model
+#'
+#' The CVPIAdata package enables users to generate the inputs for the SIT model
+#' given a begin and end year
+#'
+#' @section CVPIAdata Functions:
+#' set_SIT_data
+#'
+#' @docType package
+#' @name CVPIAdata
+NULL
--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,6 @@
 #' @name create_Sit_array
 #' @param input a vector of data, length = 252 for 12 months and 20 years of data
 #' @return 3 dimension array [location, month, year]
-#' @export
 
 create_SIT_array <- function(input) {
 
@@ -26,7 +25,6 @@ create_SIT_array <- function(input) {
 #' @param end_year int number between 1922 and 2003
 #' @param deltas bol TRUE if retrieving data for deltas, FALSE for all other reaches
 #' @return dataframe with dim(number of reaches, 240)
-#' @export
 
 spread_for_array <- function(df, variable, start_year, end_year, deltas = FALSE) {
   if (deltas) {
@@ -52,75 +50,3 @@ spread_for_array <- function(df, variable, start_year, end_year, deltas = FALSE)
   }
 
 }
-
-#' Generate SIT Model Inputs for Given 20 Year Period
-#' @description returns SIT array of temperature data for given year period
-#' @name set_SIT_data
-#' @param start_year int number between 1922 and 2003
-#' @param end_year int number between 1922 and 2003
-#' @return list of model inputs
-#' @export
-
-set_SIT_data <- function(start_year, end_year) {
-
-  if (end_year - start_year != 19) stop('years given are not a 20 year range')
-  if (end_year < start_year) stop('second year given must be after first')
-  if (end_year > 2002 | end_year < 1922) stop('last year is out of data range')
-  if (start_year < 1922 | start_year > 2002) stop('first year is out of data range')
-
-  df <- CVPIAdata::monthly_reach_data
-
-  prop_diversion <- create_SIT_array(spread_for_array(df, 'prop_diversion', start_year, end_year))
-  total_diversion <- create_SIT_array(spread_for_array(df, 'diversion', start_year, end_year))
-  prop_diversion_delta <- create_SIT_array(spread_for_array(df, 'prop_diversion', start_year, end_year, TRUE))
-  total_diversion_delta <- create_SIT_array(spread_for_array(df, 'diversion', start_year, end_year, TRUE))
-
-  temperature <- create_SIT_array(spread_for_array(df, 'avg_temp', start_year, end_year))
-  temperature_delta <- create_SIT_array(spread_for_array(df, 'avg_temp', start_year, end_year, TRUE))
-
-  # two deltas?
-  delta_inflow <- df %>%
-    dplyr::filter(watershed == 'N.Delta',
-                  year >= start_year & year <= end_year) %>%
-    dplyr::select(year, month, flow) %>%
-    tidyr::spread(year, flow) %>%
-    dplyr::select(-month)
-
-  prop_Q_sutter <- CVPIAdata::prop_Q_bypass %>%
-    dplyr::filter(year >= start_year & year <= end_year) %>%
-    dplyr::select(year, month, prop_Q_sutter) %>%
-    tidyr::spread(year, prop_Q_sutter) %>%
-    dplyr::select(-month)
-
-  prop_Q_yolo <- CVPIAdata::prop_Q_bypass %>%
-    dplyr::filter(year >= start_year & year <= end_year) %>%
-    dplyr::select(year, month, prop_Q_yolo) %>%
-    tidyr::spread(year, prop_Q_yolo) %>%
-    dplyr::select(-month)
-
-  #in-channel habitat -flow, three values per watershed (spawning, fry, parr)
-
-  #floodplain habitat -flow, 3d array [watershed, month, year]
-
-  #delta habitat -flow, one value per delta
-
-  #gate.top -flow
-
-  #DegDay -temperature, yearly value per watershed
-
-  #retQ -flow,  yearly value per watershed
-
-  #upSacQ -flow, month value per year
-
-  #egg.tmp.eff -temperature?, one value per watershed
-
-
-  return(list(p.diver = prop_diversion, t.diver = total_diversion,
-              dlt.divers = prop_diversion_delta, dlt.divers.tot = total_diversion_delta,
-              juv.tmp = temperature, juv.tmp.dlt = temperature_delta, Dlt.inf = delta_inflow,
-              prop.Q.yolo = prop_Q_yolo, prop.Q.sutter = prop_Q_sutter))
-}
-
-
-
-
