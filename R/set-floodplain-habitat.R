f40dd73f853b65e557915eb87edf7bf64c03478b
ergz <erodriguez@flowwest.com>
2017-10-13 10:09:50 -0700

    started creating approx functions for each of the floodplain

--- R/set-floodplain-habitat.R
+++ R/set-floodplain-habitat.R
@@ -1,9 +1,22 @@
+# Steps in development
+# 1. get watershed from cvpiaHabitat data package for the watershed
+# 2. get the species as defined in input (this will be mostly Fall Run)
+# 3. get wua based on the flow input
+# 4. do the interpolation to to return the appropriate are in square meters
+
+
 set_floodplain_area <- function(watershed) {
   # do stuff
 }
 
 # INTERNALS
 
-get_watershed_habitat_data <- function(w) {
-  name <- paste0()
+# set up approx functions for all floodplains
+
+american_river_floodplain <- function() {
+  d <- cvpiaHabitat::american_river_floodplain
+  f <- approxfun(d$flow_cfs, d$FR_floodplain_acres)
+
+  f
 }
+
