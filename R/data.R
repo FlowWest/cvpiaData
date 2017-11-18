4cef0bdc31216fc3ed94ee7dbbaee7ce917504bf
thesadie <sgill@flowwest.com>
2017-11-17 17:00:29 -0800

    build out documentation

--- R/data.R
+++ R/data.R
@@ -1,24 +1,3 @@
-#' Watershed Data Resolved by Month
-#'
-#' @description A dataset containing flow, diversion, and temperature data for each stream reach
-#' more info from Mike Wright regarding model used to generate data
-#' Note about supplement model use for Sutter Bypass
-#'
-#' @format A data frame with 32472 rows and 7 variables:
-#' \describe{
-#'   \item{watershed}{Stream reach element}
-#'   \item{year}{Year 1921-2003}
-#'   \item{month}{Month January to August (1-8)}
-#'   \item{flow}{Monthly average flow in cfs}
-#'   \item{diversion}{Monthly average diversion in cfs}
-#'   \item{prop_diversion}{Monthly average proportion diverted in cfs}
-#'   \item{avg_temp}{Monthly average temperature in degrees celcius}
-#'   }
-#'
-#' @source Mike Wright
-#'
-"monthly_reach_data"
-
 #' Ordering of Watersheds
 #'
 #' @format A data frame with 31 rows and 2 variables:
@@ -60,18 +39,47 @@
 #' @source Mike Urkov
 "bypass_over_top"
 
-#' Return Flow
-#' @description The proportion flows at tributary junction coming from natal
-#'   watershed.
+#' Delta Cross Channel Gates - Days Closed
+#' @description The number of days the Delta Cross Channel gates are closed for each month based on typical operation.
+#' @format dataframe with 12 rows and 2 variables:
+#' \describe{
+#' \item{month}{Integar representation of months}
+#' \item{days_closed}{the number of days the delta cross channel gates are typically closed}
+#' \item{prop_days_closed}{the proportion of days during the month that the delta cross channel gates are typically closed}
+#' }
+#' @details By rule, 45 days between November-January, based on real time monitoring.
+#' For modeling purposes, the days closed where divided between December and January.
 #'
-#' @format A data frame with 2542 rows and 4 variables: \describe{
-#'   \item{watershed}{The name of the stream reach}
-#'   \item{year}{Year 1921-2002}
-#'   \item{retQ}{The proportion flows at tributary
-#'   junction coming from natal watershed calculated from the average monthly
-#'   flow in October}
-#'   }
+#' Note: Some real-time changes possible based on:
+#' \itemize{
+#'  \item fish monitoring
+#'  \item interior delta salinity
+#'  \item flood operations
+#' }
 #'
-#'@source James T. Peterson, Mike Urkov
-'return_flow'
+#' In May, typically open for Memorial Day.
+#'
+#' @source \href{http://www.westcoast.fisheries.noaa.gov/central_valley/water_operations/ocap.html}{2009 NMFS BiOp Action IV.1 and D-1641}
+#'
+#' Compiled by Mike Urkov \email{mike.urkov@@gmail.com}
+#'
+"cross_channel_gates"
 
+#' Delta Proportion Diverted
+#' @description The proportion of delta inflow diverted
+#' 
+#' @format A 3 dimensional array: 12 by 20 by 2 (months, years, deltas)
+#' 
+#' @details 
+#' [ , , 1] North Delta
+#' 
+#' [ , , 2] South Delta
+#' 
+#' The North Delta is defined as the area west of and including the Sacramento River below Freeport to Chips Island.
+#'
+#' The South Delta is defined as the area east of the Sacramento River below Freeport to Chips Island and the San Joaquin River
+#' below Vernalis.
+#' 
+#' @source metadata contained in the cvpiaFlow package
+#' 
+"dlt_divers"
