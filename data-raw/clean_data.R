c0023bf00136bd1f44ce2e1bac41a877511bb499
thesadie <sgill@flowwest.com>
2018-01-19 10:56:21 -0800

    updated all input from adam

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,14 +1,31 @@
+library(devtools)
+library(tidyverse)
+library(grandTab)
+
+
 watershed_ordering <- readr::read_csv('data-raw/All inputs.csv') %>%
   dplyr::select(order = Order, watershed = Watershed)
 
 devtools::use_data(watershed_ordering)
 
-misc_data <- read_csv('data-raw/All inputs.csv') %>% 
-  select(order = Order, watershed = Watershed, init.adult, SCDELT, hatch.alloc, 
-         TISD, YOLO, p.tempMC2025, A.HARV, P.scour.nst, P.strand.early, P.strand.late, 
-         High.pred, contact, prop.nat)
+grandTab::grandtab %>% 
+  filter(year >= 2010, type == 'Natural') %>% 
+  mutate(watershed = replace(watershed, watershed == 'Upper-mid Sacramento River', 'Upper Sacramento River')) %>% 
+  group_by(watershed) %>% 
+  summarise(`2010-2015 mean` = mean(count, na.rm = TRUE)) %>% 
+  full_join(watershed_ordering) %>% 
+  arrange(order) %>% 
+  left_join(misc_data) %>% 
+  select(watershed, `previous value` = init.adult, `2010-2015 mean`) %>% 
+  gather(type, count, - watershed) %>% 
+  ggplot(aes(x = factor(watershed, levels = watershed_ordering$watershed), y = count, fill = type)) +
+  geom_col(position = 'dodge') +
+  coord_flip() +
+  theme_minimal() +
+  labs(x = '', y = 'initial adults') +
+  scale_fill_brewer(palette = 'Set2') +
+  theme(text = element_text(size = 20))
 
-use_data(misc_data, overwrite = TRUE)
 
 # init.adult update for new date range TODO
 # SCDELT
@@ -27,3 +44,24 @@ use_data(misc_data, overwrite = TRUE)
 #DegDay -temperature, yearly value per watershed
 #egg.tmp.eff -temperature?, one value per watershed Chris hammersmark
 
+fr <- read_csv('data-raw/All inputs FALL RUN.csv')  %>% 
+  select(order = Order, watershed = Watershed, init.adult, SCDELT, hatch.alloc, 
+         TISD, YOLO, p.tempMC2025, A.HARV, P.scour.nst, P.strand.early, P.strand.late, 
+         High.pred, contact, prop.nat.remov) %>% 
+    mutate(run = 'fall')
+
+wr <- read_csv('data-raw/All inputs WINTER RUN.csv')  %>% 
+  select(order = Order, watershed = Watershed, init.adult, SCDELT, hatch.alloc, 
+         TISD, YOLO, p.tempMC2025, A.HARV, P.scour.nst, P.strand.early, P.strand.late, 
+         High.pred, contact, prop.nat.remov) %>% 
+  mutate(run = 'winter')
+
+sr <- read_csv('data-raw/All inputs SPRING RUN.csv')  %>% 
+  select(order = Order, watershed = Watershed, init.adult, SCDELT, hatch.alloc, 
+         TISD, YOLO, p.tempMC2025, A.HARV, P.scour.nst, P.strand.early, P.strand.late, 
+         High.pred, contact, prop.nat.remov) %>% 
+  mutate(run = 'spring')
+
+misc_data <- bind_rows(fr, wr, sr)
+
+use_data(misc_data, overwrite = TRUE)
