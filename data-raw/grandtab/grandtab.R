a80e7129a1239b3055dc6a04242b11dd36f46cd5
ergz <erodriguez@flowwest.com>
2020-03-31 11:24:47 -0700

    add todo comments

--- data-raw/grandtab/grandtab.R
+++ data-raw/grandtab/grandtab.R
@@ -1,5 +1,14 @@
-install.packages("tidyverse")
 library(tidyverse)
+
+#TODO 
+# we want values for each of the watersheds these are here: 
+cvpiaData::watershed_ordering
+
+# * the dataset has watersheds under the location column
+# * battle creek is split into different locations in the dataset please consolidate into one
+# * rename mainstem as upper sacramento river 
+# * keep only the watersheds that are in the watershed_ordering dataset above
+# * the values you get here should be somewhat similar to the SimulationSeed_FallRunAdults.csv that I just added
 
 grandtab_raw <- read_csv("data-raw/grandtab/Grandtab_Modified.csv")
 
@@ -14,7 +23,7 @@ grandtab_clean <- aggregate(grandtab_yr_filter$count, by=list(grandtab_yr_filter
 
   
 
-usethis::use_data(grandtab_clean, overwrite=TRUE)
+# usethis::use_data(grandtab_clean, overwrite=TRUE)
 
 
 
