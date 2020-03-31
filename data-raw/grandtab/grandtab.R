8563d6a4f5a5ef9f270e27ad4aad807c0f1a138d
Priscilla Liang <40247074+prliang@users.noreply.github.com>
2020-03-31 11:05:37 -0700

    Average count 2013-2017

--- data-raw/grandtab/grandtab.R
+++ data-raw/grandtab/grandtab.R
@@ -1,54 +1,20 @@
-# create input for known adult seeds 
+install.packages("tidyverse")
+library(tidyverse)
+
+grandtab_raw <- read_csv("data-raw/grandtab/Grandtab_Modified.csv")
+
+grandtab_column_clean <- select(grandtab_raw, "majorbasin", "timeperiod", "count")
+ 
+grandtab_yr_filter <- filter(grandtab_column_clean, timeperiod >= '2013', timeperiod <= '2017')
+
+grandtab_clean <- aggregate(grandtab_yr_filter$count, by=list(grandtab_yr_filter$majorbasin), FUN=mean, na.rm=TRUE)
 
 
-# Known Adult Seeds --------------------------------------------------------------
-# the model is seeded by getting known adults at watersheds for each 
-# fall spring and winter.
-
-grandtab_raw <- read_csv("data-raw/grandtab/Grandtab_Modified.csv", skip = 1,
-                         col_names = c("species", "run", "majorbasin",
-                                       "minorbasin", "watershed", "timeperiod",
-                                       "startmonth", "startyear", "endmonth",
-                                       "year", "count", "data_status",
-                                       "source", "data_level", "record_type",
-                                       "div_group", "count_type"))
 
 
-# FALL RUN -----
-
-grandtab_fall_raw <- grandtab_raw %>% 
-  filter(count_type == "In-River", 
-         run == "Fall", 
-         watershed != "All") %>% 
-  select(run, year, watershed, count)
-
-# feather and yuba do not seperate counts as is needed for the model.
-# TODO come up with a better method than this eventually
-# use proportions for when the seperation was done year (2010-2012) 
-# extrapolate this proportion to all years needed in the model 
-
-# proportions for known years (2010, 2011, 2012)
-yuba_feather_prop_sr <- mean(c(0.076777295, 0.056932196, 0.081441457))
-yuba_feather_prop_fr <- 1 - yuba_feather_prop_sr
-
-yuba_and_feather_computed <- grandtab_raw %>% 
-  filter(watershed %in% c("Yuba River", "Feather River"), 
-         run %in% c("Spring", "Fall"), 
-         count_type == "In-River") %>% 
-  group_by(watershed, year) %>% 
-  summarise(
-    fall_run_total = yuba_feather_prop_fr * sum(count, na.rm = TRUE)
-  ) %>% ungroup() %>% 
-  transmute(
-    run = "Fall", 
-    year, 
-    watershed,
-    count = fall_run_total
-  )
-
-# add in these new computed values instead of the old ones
-grandtab_fall_raw_2 <- grandtab_fall_raw %>% 
-  filter(!(watershed %in% c("Yuba River", "Feather River"))) %>% 
-  bind_rows(yuba_and_feather_computed)
   
+
+usethis::use_data(grandtab_clean, overwrite=TRUE)
+
+
 
